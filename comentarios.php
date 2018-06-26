948a40735a315345e4f88afc414033133ab633ce
Nardini <leo-silva1973@hotmail.com>
2018-06-26 16:30:57 -0300

    tcc.

---
+++ comentarios.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Comentários</title>
+</head>
+<body bgcolor="#333333">//voce pode mudar a cor se quiser
+</body>
+</html>
+<table height='50' width='259' align='center'><tr><td bgcolor='#d9e6f7'><div align='center'><font color='#000000'><strong>Nome:</strong> Leonardo</font></div></td></tr><tr><td bgcolor='#d9e6f7'><div align='center'><font color='#000000'><strong>Comentário:</strong> oi</font></div></td></tr></table><table height='50' width='259' align='center'><tr><td bgcolor='#d9e6f7'><div align='center'><font color='#000000'><strong>Nome:</strong> Leonardo</font></div></td></tr><tr><td bgcolor='#d9e6f7'><div align='center'><font color='#000000'><strong>Comentário:</strong> tchau
+</font></div></td></tr></table><table height='50' width='259' align='center'><tr><td bgcolor='#d9e6f7'><div align='center'><font color='#000000'><strong>Nome:</strong> </font></div></td></tr><tr><td bgcolor='#d9e6f7'><div align='center'><font color='#000000'><strong>Comentário:</strong> </font></div></td></tr></table>
